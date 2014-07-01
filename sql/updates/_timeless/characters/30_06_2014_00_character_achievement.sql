9d5a11437f7764b7c66ea56b4d5d6bde62621903
Arci <arciatanthoria@gmail.com>
2014-07-01 22:42:18 +0200

    Database/Structure: added default value and update for deleted characters

--- sql/updates/_timeless/characters/30_06_2014_00_character_achievement.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `character_achievement` ADD COLUMN `accountID` INT(10) NOT NULL AFTER `achievement`;
-
-UPDATE `character_achievement` SET `accountID` = (SELECT `account` FROM `characters` WHERE characters.guid = character_achievement.guid LIMIT 1);
