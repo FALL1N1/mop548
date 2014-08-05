0a04154c7c97d15d5c11e200d62575d0609c924f
Arci <arciatanthoria@gmail.com>
2014-08-05 17:09:24 +0200

    Spells/Warrior: fixed damage of Dragon Roar

--- sql/updates/_timeless/core/mop_warrior.sql
+++ sql/updates/_timeless/core/mop_warrior.sql
@@ -2,3 +2,7 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (122509, -122509);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `SpellFamilyMask3`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
 (122509, 0, 4, 0, 0x00000200, 0, 0, 0x00000010, 0x0000002, 0, 0, 0);
+
+-- Dragon Roar
+DELETE FROM `spell_bonus_data` WHERE `entry`=118000;
+INSERT INTO `spell_bonus_data` VALUES (118000, 0, 0, 1.4 , 0, 'Warrior - Dragon Roar');
