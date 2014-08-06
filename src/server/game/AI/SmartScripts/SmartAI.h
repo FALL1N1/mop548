0f5da39a292ed37d3788d16caf17e4d02fc93c05
Arci <arciatanthoria@gmail.com>
2014-08-06 11:53:45 +0200

    Mics: Fixed tabs and remowed obsolote code

--- src/server/game/AI/SmartScripts/SmartAI.h
+++ src/server/game/AI/SmartScripts/SmartAI.h
@@ -57,7 +57,7 @@ class SmartAI : public CreatureAI
         void StopPath(uint32 DespawnTime = 0, uint32 quest = 0, bool fail = false);
         void EndPath(bool fail = false);
         void ResumePath();
-		void UpdatePath(const uint32 diff);
+        void UpdatePath(const uint32 diff);
         WayPoint* GetNextWayPoint();
         bool HasEscortState(uint32 uiEscortState) { return (mEscortState & uiEscortState); }
         void AddEscortState(uint32 uiEscortState) { mEscortState |= uiEscortState; }
