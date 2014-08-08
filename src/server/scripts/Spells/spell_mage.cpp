60cda509195a08ce65cecddf4cb022301d144d1a
Tanastari <quadrupedans@gmail.com>
2014-08-08 17:23:15 +0200

    Spells/Mage: Updated Conjure Refreshment and Conjure Refreshment Table.

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -31,7 +31,10 @@
 
 enum MageSpells
 {
-
+    SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R1 = 120056,
+    SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R2 = 120055,
+    SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R3 = 120054,
+    SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R4 = 120053
 };
 
 enum MageIcons
@@ -48,8 +51,114 @@ enum MiscSpells
     SPELL_PRIEST_SHADOW_WORD_DEATH                  = 32409
 };
 
+// 42955 Conjure Refreshment
+struct ConjureRefreshmentData
+{
+    uint32 minLevel;
+    uint32 maxLevel;
+    uint32 spellId;
+};
+
+uint8 const MAX_CONJURE_REFRESHMENT_SPELLS = 8;
+ConjureRefreshmentData const _conjureData[MAX_CONJURE_REFRESHMENT_SPELLS] =
+{
+    { 33, 43, 92739 },
+    { 44, 53, 92799 },
+    { 54, 63, 92802 },
+    { 64, 73, 92805 },
+    { 74, 79, 74625 },
+    { 80, 84, 92822 },
+    { 85, 89, 92727 },
+    { 90, 90, 116130 }
+};
+
+class spell_mage_conjure_refreshment : public SpellScriptLoader
+{
+public:
+    spell_mage_conjure_refreshment() : SpellScriptLoader("spell_mage_conjure_refreshment") { }
+
+    class spell_mage_conjure_refreshment_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_mage_conjure_refreshment_SpellScript);
+
+        bool Validate(SpellInfo const* /*spellInfo*/) override
+        {
+            for (uint8 i = 0; i < MAX_CONJURE_REFRESHMENT_SPELLS; ++i)
+            if (!sSpellMgr->GetSpellInfo(_conjureData[i].spellId))
+                return false;
+            return true;
+        }
+
+        bool Load() override
+        {
+            if (GetCaster()->GetTypeId() != TYPEID_PLAYER)
+                return false;
+            return true;
+        }
+
+        void HandleDummy(SpellEffIndex /*effIndex*/)
+        {
+            uint8 level = GetHitUnit()->getLevel();
+            for (uint8 i = 0; i < MAX_CONJURE_REFRESHMENT_SPELLS; ++i)
+            {
+                ConjureRefreshmentData const& spellData = _conjureData[i];
+                if (level < spellData.minLevel || level > spellData.maxLevel)
+                    continue;
+                GetHitUnit()->CastSpell(GetHitUnit(), spellData.spellId);
+                break;
+            }
+        }
+
+        void Register() override
+        {
+            OnEffectHitTarget += SpellEffectFn(spell_mage_conjure_refreshment_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const override
+    {
+        return new spell_mage_conjure_refreshment_SpellScript();
+    }
+};
+
+// 43987 Conjure Refreshment Table
+class spell_mage_conjure_refreshment_table : public SpellScriptLoader
+{
+public:
+    spell_mage_conjure_refreshment_table() : SpellScriptLoader("spell_mage_conjure_refreshment_table") { }
+
+    class spell_mage_conjure_refreshment_table_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_mage_conjure_refreshment_table_SpellScript);
+
+        void HandleBeforeCast()
+        {
+            Unit* caster = GetCaster();
+            if (caster->getLevel() < 80)
+                caster->CastSpell(caster, SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R1, true);
+            else if (caster->getLevel() < 85)
+                caster->CastSpell(caster, SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R2, true);
+            else if (caster->getLevel() < 90)
+                caster->CastSpell(caster, SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R3, true);
+            else
+                caster->CastSpell(caster, SPELL_MAGE_CONJURE_REFRESHMENT_TABLE_R4, true);
+        }
+
+        void Register()
+        {
+            // Before cast because of shared cooldown between triggering and casted spells
+            BeforeCast += SpellCastFn(spell_mage_conjure_refreshment_table_SpellScript::HandleBeforeCast);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_mage_conjure_refreshment_table_SpellScript();
+    }
+};
 
 void AddSC_mage_spell_scripts()
 {
-
+    new spell_mage_conjure_refreshment();
+    new spell_mage_conjure_refreshment_table();
 }
