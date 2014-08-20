33e66fa1435e1c0a4fb0322de54806b4a39c5cc7
Dia <Dia@github.com>
2014-08-20 23:31:19 +0200

    Spells/Priest: Fixed Body and Soul, Leap of Faith (probably) and Angelic Bulwark

---
+++ sql/updates/_timeless/core/mop_priest.sql
@@ -0,0 +1,14 @@
+-- Talent Body and Soul
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (17, 73325);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(17, "spell_pri_power_word_shield"),
+(73325, "spell_monk_crackling_jade_lightning_aura");
+
+-- Angelic Bulwark
+DELETE FROM `spell_script_names` WHERE `spell_id` = 108945;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(108945, "spell_pri_angelic_bulwark");
+
+DELETE FROM `spell_proc_event` WHERE `entry` = 108945;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `SpellFamilyMask3`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(108945, 0, 0, 0, 0, 0, 0, 0x000A22A8, 0, 0, 0, 0);
