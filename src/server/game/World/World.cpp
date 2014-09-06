d9738d9edbde2d951a8e04411a075727fd19b9b1
Najlon <karel.simanek@gmail.com>
2014-09-06 21:10:45 +0200

    Next corrections, CMSG for TIMEZONE_INFORMATION is not required and also used

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -285,6 +285,7 @@ void World::AddSession_(WorldSession* s)
     if (s->HasBoost())
         s->SendBattlePayDistributionUpdate(0, CHARACTER_BOOST, CHARACTER_BOOST_ALLOW, CHARACTER_BOOST_TEXT_ID, CHARACTER_BOOST_BONUS_TEXT, CHARACTER_BOOST_BONUS_TEXT2);
     s->SendTutorialsData();
+    s->SendTimezoneInformation();
 
     UpdateMaxSessionCounters();
 
