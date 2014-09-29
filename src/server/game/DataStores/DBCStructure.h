c7db5e697b234af2e6bff591616a6882a3e47cd1
moJIto-ice <akriso@i.ua>
2014-09-29 10:54:31 +0400

    fix crash from CreateInstance when entering Scenario, add instanceTemplate for all mop raids, dungeons, scenarios

--- src/server/game/DataStores/DBCStructure.h
+++ src/server/game/DataStores/DBCStructure.h
@@ -1861,7 +1861,7 @@ struct MapEntry
     // Helpers
     uint32 Expansion() const { return addon; }
 
-    bool IsDungeon() const { return map_type == MAP_INSTANCE || map_type == MAP_RAID; }
+    bool IsDungeon() const { return map_type == MAP_INSTANCE || map_type == MAP_RAID || map_type == MAP_SCENARIO; }
     bool IsNonRaidDungeon() const { return map_type == MAP_INSTANCE; }
     bool Instanceable() const { return map_type == MAP_INSTANCE || map_type == MAP_RAID || map_type == MAP_BATTLEGROUND || map_type == MAP_ARENA || map_type == MAP_SCENARIO; }
     bool IsRaid() const { return map_type == MAP_RAID; }
