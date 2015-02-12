83b9aabe4b1a9e53b230002d9a05a4a2050d246a
zzhelyazkov <dirtyleet@gmail.com>
2015-02-12 02:21:46 +0200

    Add sql

---
+++ sql/forgottenlands/world/2015_02_11_spell_script_names.sql
@@ -0,0 +1,14 @@
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_warr_sudden_death';
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES (29725, 'spell_warr_sudden_death');
+DELETE FROM spell_script_names WHERE ScriptName IN ('spell_warr_heroic_leap_damage', 'spell_warr_heroic_leap', 'spell_warr_colossus_smash', 'spell_warr_second_wind',  'spell_warr_glyph_of_hindering_strikes', 'spell_warr_charge', 'spell_warr_glyph_of_hamstring', 'spell_warr_staggering_shout');
+INSERT INTO spell_script_names (spell_id, ScriptName) VALUES 
+(52174, 'spell_warr_heroic_leap_damage'),
+(6544, 'spell_warr_heroic_leap'),
+(86346, 'spell_warr_colossus_smash'),
+(29838, 'spell_warr_second_wind'),
+(78, 'spell_warr_glyph_of_hindering_strikes'),
+(845, 'spell_warr_glyph_of_hindering_strikes'),
+(100, 'spell_warr_charge'),
+(1715, 'spell_warr_glyph_of_hamstring'),
+(107566, 'spell_warr_staggering_shout');
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_warl_create_healthstone';
