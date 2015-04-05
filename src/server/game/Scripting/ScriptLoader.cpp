e77eb16ba0166eb5648fb00395f80d5918d887df
Emiliyan Kurtseliyanski <emiliyan9604bg@gmail.com>
2015-04-05 13:16:14 +0300

    Import mastery spells, thanks to @NoFearDeathProject

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -43,6 +43,7 @@ void AddSC_quest_spell_scripts();
 void AddSC_item_spell_scripts();
 void AddSC_example_spell_scripts();
 void AddSC_holiday_spell_scripts();
+void AddSC_masteries_spell_scripts();
 
 void AddSC_SmartSCripts();
 
@@ -862,6 +863,7 @@ void AddSpellScripts()
     AddSC_item_spell_scripts();
     AddSC_example_spell_scripts();
     AddSC_holiday_spell_scripts();
+    AddSC_masteries_spell_scripts();
 }
 
 void AddCommandScripts()
